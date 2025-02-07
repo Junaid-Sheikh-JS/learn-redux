61bc606b3da89ab35a2c4aac420ee747155d3573
m junaid <junaidislam441@gmail.com>
2025-02-07 18:22:35 +0500

    updation

--- src/store.js
+++ src/store.js
@@ -32,9 +32,22 @@ export default taskReducer;
 const store = createStore(taskReducer)
 console.log("initial: ", store.getState());
 
-store.dispatch({ type: AddTask, payload: "Eat food" })
+store.dispatch(addtask("Eat food"))
 console.log("updated state: ", store.getState());
-store.dispatch({ type: AddTask, payload: "do your work" })
+
+// store.dispatch({ type: AddTask, payload: "do your work" })
+store.dispatch(addtask("do your work"))
 console.log("updated state: ", store.getState());
-store.dispatch({ type: DeleteTask, payload: 1 })
+
+// store.dispatch({ type: DeleteTask, payload: 1 })
+store.dispatch(delTask())
 console.log("deleted task: ", store.getState())
+
+
+// create action creator
+function addtask(data) {
+    return { type: AddTask, payload: data }
+}
+function delTask() {
+    return { type: DeleteTask, payload: 0 }
+}
